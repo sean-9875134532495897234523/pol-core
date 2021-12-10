f18c694bf703278aea0120604fcd0d38dd116438
Sean Smith <sean_smith@tutanota.com>
2021-12-10 11:24:07 +0000

    Reconfigure environmental variables

--- environment/environment-config.ts
+++ environment/environment-config.ts
@@ -1,5 +1,20 @@
-import { config } from "https://deno.land/x/dotenv/mod.ts";
 
 const isDenoDeploy = Deno.env.get("DENO_DEPLOYMENT_ID") !== undefined;
 
-export const configure = isDenoDeploy ? () => {} : config;
+console.log('DENO_DEPLOYMENT_ID: ' + Deno.env.get("DENO_DEPLOYMENT_ID"));
+console.log('isDenoDeploy: ' + isDenoDeploy);
+
+async function loadLocalEnvironmentVariables() {
+    const text = await Deno.readTextFile("./environment/local_env_vars.json");
+    const envVars : Record<string, unknown> = JSON.parse(text);
+    for (const setting in envVars) {
+        console.log(setting, envVars[setting]);
+        Deno.env.set(setting, envVars[setting] as string);
+    }
+}
+
+export function configure() {
+    if (!isDenoDeploy) {
+        loadLocalEnvironmentVariables();
+    }
+}
